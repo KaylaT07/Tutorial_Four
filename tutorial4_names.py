c853a029adb17f31244537c0c9ae521fec2cc874
KaylaT07 <aaliyahtrotz12@gmail.com>
2025-10-22 09:38:50 -0400

    Delete tutorial4_names.py

--- tutorial4_names.py
+++
@@ -1,9 +0,0 @@
-# tutorial4_names.py
-# This program prints the names of both collaborators for Tutorial 4
-
-def main():
-    print("Aaliyah Trotz")
-    print("Cleon Adonis")
-
-if __name__ == "__main__":
-    main()
